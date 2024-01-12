b219cd2972c962420fdd94a1b78696954d001c01
meshafi <shafiaqueel@gmail.com>
2024-01-12 11:10:38 +0530

    fixed schema

--- app/schemas/employee_schema.py
+++ app/schemas/employee_schema.py
@@ -5,7 +5,7 @@ class EmployeeSchema(Schema):
     name = fields.String(required=True)
     position = fields.String(required=True)
     department = fields.String(required=True)
-    email = fields.Email(required=True)
+    email = fields.String(required=True)
     phone = fields.Integer(validate=validate.Range(min=0, max=9999999999), required=True)
     address = fields.String(required=True)
     hireDate = fields.String(required=True) 
