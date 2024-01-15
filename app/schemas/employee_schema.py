8e20c954f81c7a7f6b43a6c5e54e33ccc657043f
meshafi <shafiaqueel@gmail.com>
2024-01-15 16:28:10 +0530

    removed pycache files

--- app/schemas/employee_schema.py
+++ app/schemas/employee_schema.py
@@ -1,7 +1,7 @@
 from marshmallow import Schema, fields, validate
 
 class EmployeeSchema(Schema):
-    _id = fields.String(attribute='_id', dump_only=True)  # Assuming '_id' is the field in your documents
+    _id = fields.String(attribute='_id', dump_only=True)  
     name = fields.String(required=True)
     position = fields.String(required=True)
     department = fields.String(required=True)
